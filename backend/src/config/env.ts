354bcb77da11f79f42dd5130c254ff6ac71bb9d9
vegAs-RW <jordan.pouchot2@gmail.com>
2024-06-03 15:59:27 +0200

    :wrench: Update config for drizzle orm config

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -2,7 +2,8 @@ import { EnvConfig } from "../types/env";
 
 const env: EnvConfig = {
     PORT: parseInt(process.env.PORT || "8000"),
-    HOST: process.env.HOST || "localhost"
+    HOST: process.env.HOST || "localhost",
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres:admin@localhost:5432/mydb"
 }
 
 export default env;
