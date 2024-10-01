e8eca2f042d939f55c84572bc5f1c5353da111bd
vegAs-RW <jordan.pouchot2@gmail.com>
2024-10-01 14:31:21 +0200

    :card_file_box:Update user schema

--- backend/src/infrastructure/data/schema/users.ts
+++ backend/src/infrastructure/data/schema/users.ts
@@ -6,5 +6,4 @@ export const users = pgTable('users', {
     firstname: varchar('firstname', {length: 255}).notNull(),
     email: varchar('email', {length: 255}).notNull(),
     password: varchar('password', {length: 255}).notNull(),
-    role: varchar('role', {length: 255}).notNull() 
 })
