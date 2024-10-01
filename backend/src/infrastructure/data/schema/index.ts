4604a8ec657474dc7341248ae2f285b7857885fe
vegAs-RW <jordan.pouchot2@gmail.com>
2024-10-01 10:24:10 +0200

    :card_file_box:Combine all schemas in index.ts

---
+++ backend/src/infrastructure/data/schema/index.ts
@@ -0,0 +1,15 @@
+import {users} from "./users";
+import {advisors} from "./advisors";
+import {establishments} from "./establishments";
+import { laboratories } from "./laboratories";
+import {directors} from "./laboratoriesDirectors";
+import { theses } from "./thesis";
+
+export {
+    users,
+    advisors,
+    establishments,
+    laboratories,
+    directors,
+    theses
+}
