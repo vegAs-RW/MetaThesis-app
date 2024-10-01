8d2b58d5184209207f2f2206c9a9e0233c98ca17
vegAs-RW <jordan.pouchot2@gmail.com>
2024-10-01 12:16:32 +0200

    :closed_lock_with_key:Update env variable for authentification

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -3,7 +3,8 @@ import { EnvConfig } from "../types/env";
 const env: EnvConfig = {
     PORT: parseInt(process.env.PORT || "8000"),
     HOST: process.env.HOST || "localhost",
-    DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres:admin@localhost:5432/mydb"
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres:admin@localhost:5432/mydb",
+    JWT_SECRET: process.env.JWT_SECRET || "SuperSecretKey"
 }
 
 export default env;
--- backend/src/types/env.ts
+++ backend/src/types/env.ts
@@ -2,4 +2,5 @@ export interface EnvConfig {
     PORT: number;
     HOST: string;
     DATABASE_URL: string;
+    JWT_SECRET: string;
 }
