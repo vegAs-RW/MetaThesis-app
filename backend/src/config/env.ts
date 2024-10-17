27c92a59f46cf0609fcb332bcd955eec854bea7a
vegAs-RW <jordan.pouchot2@gmail.com>
2024-10-17 16:54:17 +0200

    :lock: Update config

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -3,7 +3,7 @@ import { EnvConfig } from "../types/env";
 const env: EnvConfig = {
     PORT: parseInt(process.env.PORT || "8000"),
     HOST: process.env.HOST || "localhost",
-    DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres:admin@localhost:5432/mydb",
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres:JordanAlex11@localhost:5432/metaThesis",
     JWT_SECRET: process.env.JWT_SECRET || "SuperSecretKey"
 }
 
