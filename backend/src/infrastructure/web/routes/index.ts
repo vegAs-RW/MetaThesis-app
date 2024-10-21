af78ab15a6b5e13c72a30c0910fc5ddd0fb37139
vegAs-RW <jordan.pouchot2@gmail.com>
2024-10-21 10:36:25 +0200

    :necktie: Update router for laboratory routes

--- backend/src/infrastructure/web/routes/index.ts
+++ backend/src/infrastructure/web/routes/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import authRoutes from './authRoutes';
 import userRoutes from './userRoutes';
+import laboratoryRoutes from './laboratoryRoutes';
 
 import { isAuth } from '../../../middlewares/authMiddleware';
 
@@ -10,5 +11,7 @@ const router = express.Router();
 router.use('/auth', authRoutes);
 
 router.use('/user', isAuth, userRoutes);
+
+router.use('/laboratory', isAuth, laboratoryRoutes);
 
 export default router;
