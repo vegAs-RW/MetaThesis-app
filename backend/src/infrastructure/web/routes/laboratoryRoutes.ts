7502c3e63036b591caaac0a7179c00b615dfb5ba
vegAs-RW <jordan.pouchot2@gmail.com>
2024-10-21 11:13:15 +0200

    :necktie: Update laboratory router for middleware

--- backend/src/infrastructure/web/routes/laboratoryRoutes.ts
+++ backend/src/infrastructure/web/routes/laboratoryRoutes.ts
@@ -1,15 +1,14 @@
 import express from 'express';
 import { createLaboratory, updateLaboratory, getAllLaboratories, getLaboratoryByName, getLaboratoryByCity, getLaboratoryByCountry, getLaboratoryById } from '../controllers/LaboratoryController';
-import { isAuth } from '../../../middlewares/authMiddleware';
 
 const router = express.Router();
 
-router.post("/", isAuth, createLaboratory);
-router.put("/:id", isAuth, updateLaboratory);
-router.get("/", isAuth, getAllLaboratories);
-router.get("/:id", isAuth, getLaboratoryById);
-router.get("/name/:name", isAuth, getLaboratoryByName);
-router.get("/city/:city", isAuth, getLaboratoryByCity);
-router.get("/country/:country", isAuth, getLaboratoryByCountry);
+router.post("/", createLaboratory);
+router.put("/:id", updateLaboratory);
+router.get("/", getAllLaboratories);
+router.get("/:id", getLaboratoryById);
+router.get("/name/:name", getLaboratoryByName);
+router.get("/city/:city", getLaboratoryByCity);
+router.get("/country/:country", getLaboratoryByCountry);
 
 export default router;
