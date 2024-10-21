6d1f61d10f3197ae875926c9a4466a5ed8a50767
vegAs-RW <jordan.pouchot2@gmail.com>
2024-10-21 16:15:50 +0200

    :card_file_box: Create Db schema for candidate entity

---
+++ backend/src/infrastructure/data/schema/candidates.ts
@@ -0,0 +1,18 @@
+
+import { pgTable, uuid, varchar, date, boolean } from "drizzle-orm/pg-core";
+import { advisors } from "./advisors";
+
+export const candidates = pgTable("candidates", {
+    id: uuid("id").defaultRandom().primaryKey(),
+    lastname: varchar("lastname", { length: 255 }).notNull(),
+    firstname: varchar("firstname", { length: 255 }).notNull(),
+    birthdate: date("birthdate").notNull(),
+    lastDegree: varchar("last_degree", { length: 255 }).notNull(),
+    dateLastDegree: date("date_Last_Degree").notNull(),
+    doctoralSchool: varchar("doctoral_school", { length: 255 }).notNull(),
+    residentPermit: varchar("resident_permit", { length: 255 }).notNull(),
+    committeeValidation: boolean("committee_validation"),
+    hrValidation: boolean("hr_validation"),
+    zrrValidation: boolean("zrr_validation"),
+    advisor: uuid("advisors").references(() => advisors.id).notNull(),
+    });
