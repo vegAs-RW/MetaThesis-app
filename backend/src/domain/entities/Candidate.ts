ae7b99e9f9f5a1ccf49b19887cf18cc31e985a6c
vegAs-RW <jordan.pouchot2@gmail.com>
2024-10-23 12:20:49 +0200

    :necktie: Update entities for new type

--- backend/src/domain/entities/Candidate.ts
+++ backend/src/domain/entities/Candidate.ts
@@ -6,3 +6,5 @@ export type Candidate = InferSelectModel<typeof candidates>;
 export type NewCandidate = InferInsertModel<typeof candidates>;
 
 export type CandidateColumns = {[K in keyof Candidate]?:boolean};
+
+export type CandidateWithAdvisor = Candidate & {advisorInfo: {department: string, research_area: string, ifrs: string, costCenter: string} | null};
