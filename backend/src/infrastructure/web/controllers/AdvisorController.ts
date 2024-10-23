d349086d81037b59e67315c13a57ec47f4088b06
vegAs-RW <jordan.pouchot2@gmail.com>
2024-10-23 17:22:44 +0200

    :bulb: Add comment

--- backend/src/infrastructure/web/controllers/AdvisorController.ts
+++ backend/src/infrastructure/web/controllers/AdvisorController.ts
@@ -5,11 +5,20 @@ import { APIResponse } from "../../../utils/APIResponse";
 
 const advisorService = new AdvisorService();
 
+/**
+ * Controller to handle the updating of an advisor.
+ * @param {Request} req - The request object from the client, containing the advisor ID and updated data.
+ * @param {Response} res - The response object to send data back to the client.
+ */
 export const updateAdvisor = async (req: Request, res: Response) => {
     try {
+        // Extracting the advisor ID from the request parameters
         const { id } = req.params;
 
+        // Extracting the updated data from the request body
         const { department, research_area, ifrs, costCenter, establishment, email, firstname, lastname, password, role } = req.body;
+
+        // Fetching the advisor from the database by ID
         const advisor = await advisorService.getAdvisorById(id, {
             id: true,
             email: true,
@@ -23,8 +32,11 @@ export const updateAdvisor = async (req: Request, res: Response) => {
             costCenter: true,
             establishment: true,
         });
+
+        // If the advisor is not found, return a 404 error
         if (!advisor) return APIResponse(res, { statusCode: 404, message: "Advisor not found" });
 
+        // Constructing the updated advisor object with new values or existing ones if not provided
         const updatedAdvisor: Advisor = {
             id,
             department: department || advisor.department,
@@ -38,14 +50,17 @@ export const updateAdvisor = async (req: Request, res: Response) => {
             password: password,
             role: role
         };
+        // Updating the advisor using the AdvisorService
         await advisorService.updateAdvisor(updatedAdvisor);
 
+        // Sending a success response
         APIResponse(res, {
             statusCode: 200,
             message: "Advisor updated successfully",
         });
 
     } catch (error) {
+        // Logging the error and returning a 500 response in case of an exception
         console.error(error);
         return APIResponse(res, { statusCode: 500, message: "An error occurred while updating advisor" });
     }
--- backend/src/infrastructure/web/controllers/AuthController.ts
+++ backend/src/infrastructure/web/controllers/AuthController.ts
@@ -9,22 +9,33 @@ const authService = new AuthService();
 const userService = new UserService();
 const advisorService = new AdvisorService();
 
+/**
+ * Controller to handle the registration of a new advisor.
+ * @param {Request} req - The request object containing the advisor's registration data.
+ * @param {Response} res - The response object to send data back to the client.
+ */
 export const registerAdvisor = async (req: Request, res: Response) => {
     try {
+        // Destructuring the necessary fields from the request body
         const { email, password, firstname, lastname, department, research_area, ifrs, costCenter, establishment } = req.body;
+        // Input validation for email and password
         if (!email.trim() || !password.trim() || typeof email !== "string" || typeof password !== "string") {
             return APIResponse(res, {
                 statusCode: 400,
                 message: "Invalid email or password",
             });
         }
+        // Checking if the user already exists
         const existingUser = await userService.getUserByEmail(email, { email: true });
         if (existingUser) return APIResponse(res, { statusCode: 409, message: "Advisor already exists" });
 
+        // Hashing the password for secure storage
         const hashedPassword = await bcrypt.hash(password, 12);
 
+        // Creating a new user in the system
         const newUser = await userService.createUser({ email, password: hashedPassword, lastname, firstname });
 
+        // Handling the case where user creation fails
         if (!newUser || !newUser.id) {
             return APIResponse(res, {
                 statusCode: 500,
@@ -32,57 +43,74 @@ export const registerAdvisor = async (req: Request, res: Response) => {
             });
         }
 
+        // Creating an advisor linked to the new user
         await advisorService.createAdvisor({
             id: newUser.id,
-            department, 
-            research_area, 
-            ifrs, 
-            costCenter, 
+            department,
+            research_area,
+            ifrs,
+            costCenter,
             establishment,
             email: newUser.email,
-            firstname: newUser.firstname, // Inclure le prénom de l'utilisateur
-            lastname: newUser.lastname, // Inclure le nom de famille de l'utilisateur
-            password: hashedPassword, // Inclure le mot de passe (si nécessaire)
+            firstname: newUser.firstname, 
+            lastname: newUser.lastname, 
+            password: hashedPassword, 
             role: "advisor",
         });
 
+        // Sending a success response
         APIResponse(res, {
             statusCode: 201,
             message: "Advisor created successfully",
         });
 
     } catch (error) {
+        // Logging the error and sending a generic error response
         console.error(error);
         return APIResponse(res, { statusCode: 500, message: "An error occurred while creating advisor" });
     }
 }
 
+/**
+ * Controller to handle user login.
+ * @param {Request} req - The request object containing login credentials.
+ * @param {Response} res - The response object to send data back to the client.
+ */
 export const login = async (req: Request, res: Response) => {
     try {
+        // Destructuring email and password from the request body
         const { email, password } = req.body;
+        // Input validation for email and password
         if (!email.trim() || !password.trim() || typeof email !== "string" || typeof password !== "string") {
             return APIResponse(res, {
                 statusCode: 400,
                 message: "Invalid email or password",
             });
         }
+        // Fetching user details by email
         const user = await userService.getUserByEmail(email, { email: true, password: true });
 
+        // Handling case where user is not found
         if (!user) return APIResponse(res, { statusCode: 404, message: "User not found" });
 
+        // Validating the provided password against the stored hashed password
         const isPasswordValid = await bcrypt.compare(password, user.password as string);
 
+        // Handling invalid password case
         if (!isPasswordValid) return APIResponse(res, { statusCode: 401, message: "Invalid password" });
 
+        // Creating a token for the user
         const token = authService.createToken(user.id as string);
 
-        res.cookie("token", token, { 
+        // Setting a secure cookie with the token
+        res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production"
             /*sameSite: "strict"
             maxAge: 24  * 60 * 60 * 1000*/
         });
 
+        // Sending a success response with the token
         APIResponse(res, {
             statusCode: 200,
             message: "Login successful",
@@ -90,20 +118,30 @@ export const login = async (req: Request, res: Response) => {
         });
 
     } catch (error) {
+        // Logging the error and sending a generic error response
         console.error(error);
-        APIResponse(res, { statusCode: 500, message: "An error occurred while logging in"
+        APIResponse(res, {
+            statusCode: 500, message: "An error occurred while logging in"
         });
     }
 }
 
+/**
+ * Controller to handle user logout.
+ * @param {Request} req - The request object for logout action.
+ * @param {Response} res - The response object to send data back to the client.
+ */
 export const logout = async (req: Request, res: Response) => {
     try {
+        // Clearing the authentication token cookie
         res.clearCookie("token");
+        // Sending a success response
         APIResponse(res, {
             statusCode: 200,
             message: "Logout successful"
         });
     } catch (error) {
+        // Logging the error and sending a generic error response
         console.error(error);
         APIResponse(res, { statusCode: 500, message: "An error occurred while logging out" });
     }
