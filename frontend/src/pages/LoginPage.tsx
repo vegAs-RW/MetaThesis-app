14814dbcfbe17be13fdc3b52efbf8e046a0c24ac
vegAs-RW <jordan.pouchot2@gmail.com>
2024-10-25 17:09:51 +0200

    :construction: Update Auth pages & add style

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,6 +1,5 @@
-// src/pages/LoginPage.tsx
 import React, { useState } from 'react';
-import api from '../services/api'; // Import du service ici
+import api from '../services/api';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { login } from '../redux/authSlice';
@@ -18,10 +17,16 @@ const LoginPage: React.FC = () => {
 
     try {
       const response = await api.post('/auth/login', { email, password });
-      const {token, role} = response.data.data;
+      const { token, role } = response.data.data;
       if (response.data.message === 'Login successful') {
         dispatch(login({ token, role }));
-        navigate('/');
+        if (role === 'admin') {
+          navigate('/dashboard/admin');
+        } else if (role === 'advisor') {
+          navigate('/dashboard/advisor');
+        } else {
+          setError('Probleme d\'autorisation');
+        }
       } else {
         setError('Email ou mot de passe incorrect');
       }
@@ -31,30 +36,39 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>Connexion</h1>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Email :</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Mot de passe :</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <button type="submit">Se connecter</button>
-      </form>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-blue-200 to-blue-500">
+      <div className="w-full max-w-lg px-8 py-6 bg-white rounded-lg shadow-lg transform transition-all duration-500 hover:shadow-2xl md:px-10 md:py-8 lg:max-w-md">
+        <h1 className="text-3xl font-bold text-center text-blue-600 mb-8">Connexion</h1>
+        <form onSubmit={handleLogin} className="space-y-6">
+          <div>
+            <label className="block text-md font-semibold text-gray-600">Email :</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </div>
+          <div>
+            <label className="block text-md font-semibold text-gray-600">Mot de passe :</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </div>
+          {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+          <button
+            type="submit"
+            className="w-full py-3 mt-4 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-300 shadow-lg focus:ring-4 focus:ring-blue-400 focus:outline-none"
+          >
+            Se connecter
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import api from '../services/api'; // Assurez-vous que ce service Axios est correctement configuré
+import api from '../services/api';
 import { useNavigate } from 'react-router-dom';
 
 const SignupPage: React.FC = () => {
-  // États pour chaque champ du formulaire
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [firstname, setFirstname] = useState('');
@@ -16,7 +15,6 @@ const SignupPage: React.FC = () => {
   const [selectedEstablishment, setSelectedEstablishment] = useState(''); // Établissement sélectionné
   const navigate = useNavigate();
 
-  // Fonction pour récupérer les établissements depuis l'API
   const fetchEstablishments = async () => {
     try {
       const response = await api.get('/establishment'); // Requête vers l'API pour obtenir les établissements
@@ -27,41 +25,38 @@ const SignupPage: React.FC = () => {
     }
   };
 
-  // Utilisation de useEffect pour déclencher la récupération des établissements au montage du composant
   useEffect(() => {
-    fetchEstablishments(); // Appel de la fonction pour charger les établissements
+    fetchEstablishments(); 
   }, []);
 
-  // Fonction pour gérer la soumission du formulaire
   const handleSignup = async (e: React.FormEvent) => {
-    e.preventDefault(); // Empêche le rechargement de la page
+    e.preventDefault(); 
 
     try {
-        console.log("Données d'inscription :", {
-            email,
-            password,
-            firstname,
-            lastname,
-            department,
-            researchArea,
-            ifrs,
-            costCenter,
-            selectedEstablishment,
-        });
-      // Requête POST pour envoyer les données d'inscription à l'API
+      console.log("Données d'inscription :", {
+        email,
+        password,
+        firstname,
+        lastname,
+        department,
+        researchArea,
+        ifrs,
+        costCenter,
+        selectedEstablishment,
+      });
+      
       await api.post('/auth/register', {
         email,
         password,
         firstname,
         lastname,
         department,
-        research_area: researchArea, // Nom du champ correspond à ce qui est attendu dans l'API
+        research_area: researchArea, 
         ifrs,
         costCenter,
-        establishment: selectedEstablishment, // On envoie l'ID de l'établissement sélectionné
+        establishment: selectedEstablishment, 
       });
 
-      // Redirection vers la page de connexion après succès
       navigate('/login');
     } catch (error) {
       console.error("Erreur lors de l'inscription :", error);
@@ -69,99 +64,114 @@ const SignupPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>Inscription</h1>
-      <form onSubmit={handleSignup}>
-        <div>
-          <label>Email</label>
-          <input 
-            type="email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-            required 
-          />
-        </div>
-        <div>
-          <label>Mot de passe</label>
-          <input 
-            type="password" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-            required 
-          />
-        </div>
-        <div>
-          <label>Prénom</label>
-          <input 
-            type="text" 
-            value={firstname} 
-            onChange={(e) => setFirstname(e.target.value)} 
-            required 
-          />
-        </div>
-        <div>
-          <label>Nom</label>
-          <input 
-            type="text" 
-            value={lastname} 
-            onChange={(e) => setLastname(e.target.value)} 
-            required 
-          />
-        </div>
-        <div>
-          <label>Département</label>
-          <input 
-            type="text" 
-            value={department} 
-            onChange={(e) => setDepartment(e.target.value)} 
-            required 
-          />
-        </div>
-        <div>
-          <label>Zone de recherche</label>
-          <input 
-            type="text" 
-            value={researchArea} 
-            onChange={(e) => setResearchArea(e.target.value)} 
-            required 
-          />
-        </div>
-        <div>
-          <label>IFRS</label>
-          <input 
-            type="text" 
-            value={ifrs} 
-            onChange={(e) => setIfrs(e.target.value)} 
-            required 
-          />
-        </div>
-        <div>
-          <label>Centre de coût</label>
-          <input 
-            type="text" 
-            value={costCenter} 
-            onChange={(e) => setCostCenter(e.target.value)} 
-            required 
-          />
-        </div>
-        <div>
-          <label>Établissement</label>
-          <select
-            value={selectedEstablishment}
-            onChange={(e) => setSelectedEstablishment(e.target.value)}
-            required
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-blue-200 to-blue-500">
+      <div className="w-full max-w-lg px-8 py-6 bg-white rounded-lg shadow-lg transform transition-all duration-500 hover:shadow-2xl md:px-10 md:py-8 lg:max-w-md">
+        <h1 className="text-3xl font-bold text-center text-blue-600 mb-8">Inscription</h1>
+        <form onSubmit={handleSignup} className="space-y-6">
+          <div>
+            <label className="block text-md font-semibold text-gray-600">Email :</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </div>
+          <div>
+            <label className="block text-md font-semibold text-gray-600">Mot de passe :</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </div>
+          <div>
+            <label className="block text-md font-semibold text-gray-600">Prénom :</label>
+            <input
+              type="text"
+              value={firstname}
+              onChange={(e) => setFirstname(e.target.value)}
+              required
+              className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </div>
+          <div>
+            <label className="block text-md font-semibold text-gray-600">Nom :</label>
+            <input
+              type="text"
+              value={lastname}
+              onChange={(e) => setLastname(e.target.value)}
+              required
+              className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </div>
+          <div>
+            <label className="block text-md font-semibold text-gray-600">Organisation :</label>
+            <input
+              type="text"
+              value={department}
+              onChange={(e) => setDepartment(e.target.value)}
+              required
+              className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </div>
+          <div>
+            <label className="block text-md font-semibold text-gray-600">Service :</label>
+            <input
+              type="text"
+              value={researchArea}
+              onChange={(e) => setResearchArea(e.target.value)}
+              required
+              className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </div>
+          <div>
+            <label className="block text-md font-semibold text-gray-600">Code IFRS :</label>
+            <input
+              type="text"
+              value={ifrs}
+              onChange={(e) => setIfrs(e.target.value)}
+              required
+              className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </div>
+          <div>
+            <label className="block text-md font-semibold text-gray-600">Centre de coût :</label>
+            <input
+              type="text"
+              value={costCenter}
+              onChange={(e) => setCostCenter(e.target.value)}
+              required
+              className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+          </div>
+          <div>
+            <label className="block text-md font-semibold text-gray-600">Établissement :</label>
+            <select
+              value={selectedEstablishment}
+              onChange={(e) => setSelectedEstablishment(e.target.value)}
+              required
+              className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+            >
+              <option value="">Sélectionner un établissement</option>
+              {establishments.map((establishment: { id: string; name: string }) => (
+                <option key={establishment.id} value={establishment.id}>
+                  {establishment.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button
+            type="submit"
+            className="w-full py-3 mt-4 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-300 shadow-lg focus:ring-4 focus:ring-blue-400 focus:outline-none"
           >
-            <option value="">Sélectionner un établissement</option>
-            {/* Affichage dynamique des établissements récupérés */}
-            {establishments.map((establishment: { id: string; name: string }) => (
-              <option key={establishment.id} value={establishment.id}>
-                {establishment.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <button type="submit">S'inscrire</button>
-      </form>
+            S'inscrire
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
