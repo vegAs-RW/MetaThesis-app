ccd193453f4ee6adae9a937a3b906426628f4656
vegAs-RW <jordan.pouchot2@gmail.com>
2024-10-25 17:11:03 +0200

    :construction: Update router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
-import './App.css'
+import DashboardAdvisor from './pages/AdvisorDashboard'
+import CreateThesis from './pages/CreateThesis'
+import EditThesis from './pages/EditThesis'
+import './style/tailwind.css'
+import Layout from './components/Layout'
 
 const App: React.FC = () => {
   return (
     
       <Router>
+        <Layout>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
+          <Route path="/dashboard/advisor" element={<DashboardAdvisor />} />
+          <Route path="/create-thesis" element={<CreateThesis />} />
+          <Route path="/thesis/edit/:thesisId" element={<EditThesis />} />
         </Routes>
+        </Layout>
     </Router>
       
   
