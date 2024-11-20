875fc2a6bc9c006d8c39cb5d860d4d56e711c0ec
vegAs-RW <jordan.pouchot2@gmail.com>
2024-11-20 17:53:24 +0100

    :necktie: Add EntityDetailsModal

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -1,9 +1,10 @@
-// AllTheses.tsx
 import React, { useEffect, useState } from "react";
 import api from "../services/api";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import ThesisFilters from "../components/ThesisFilter";
+import * as XLSX from "xlsx";
+import EntityDetailsModal from "../components/EntityDetailsModal";
 
 interface Thesis {
   id: string;
@@ -16,15 +17,34 @@ interface Thesis {
     id: string;
     firstname: string;
     lastname: string;
+    costCenter: string;
+    IFRS: string;
+    establishment: {
+      id: string;
+      name: string;
+      address: string;
+      city: string;
+      siret: string;
+      zipcode: string;
+      telephone: string;
+    };
   };
   candidate?: {
     id: string;
     firstname: string;
     lastname: string;
+    birthdate: string;
+    lastDegree: string;
+    dateLastDegree: string;
   };
   laboratory?: {
     id: string;
     name: string;
+    address: string;
+    city: string;
+    country: string;
+    means: string;
+    expertise: string;
   };
 }
 
@@ -33,6 +53,9 @@ const AdminDashboard: React.FC = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
   const [filters, setFilters] = useState({});
+  const [selectedThesisId, setSelectedThesisId] = useState<string | null>(null);
+  const [showModal, setShowModal] = useState(false);
+
   const token = useSelector((state: RootState) => state.auth.token);
 
   const fetchTheses = async (filters: any) => {
@@ -62,10 +85,50 @@ const AdminDashboard: React.FC = () => {
     setFilters(newFilters);
   };
 
+  const handleThesisClick = (thesisId: string) => {
+    setSelectedThesisId(thesisId);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedThesisId(null);
+  };
+
+  // Fonction pour exporter les données en Excel
+  const exportToExcel = () => {
+    const data = theses.map((thesis) => ({
+      Advisor: `${thesis.advisor.firstname} ${thesis.advisor.lastname}`,
+      Topic: thesis.topic,
+      Year: thesis.year,
+      Domain: thesis.domain,
+      ScientistInterest: thesis.scientistInterest,
+      Keyword: thesis.keyword,
+      Candidate: thesis.candidate
+        ? `${thesis.candidate.firstname} ${thesis.candidate.lastname}`
+        : "No entry",
+      Laboratory: thesis.laboratory ? thesis.laboratory.name : "No entry",
+    }));
+
+    // Créer une feuille de calcul à partir des données
+    const ws = XLSX.utils.json_to_sheet(data);
+    const wb = XLSX.utils.book_new();
+
+    // Ajouter la feuille de calcul au classeur
+    XLSX.utils.book_append_sheet(wb, ws, "Theses");
+
+    // Télécharger le fichier Excel
+    XLSX.writeFile(wb, "Theses.xlsx");
+  };
+
+  const handleEntityUpdated = async () => {
+    await fetchTheses(filters); // Appelez fetchTheses sans argument
+  };
+
+  if (selectedThesisId) console.log("Thèses:", selectedThesisId);
+
   if (loading) {
-    return (
-      <p className="text-center text-gray-600">Loading Theses...</p>
-    );
+    return <p className="text-center text-gray-600">Loading Theses...</p>;
   }
 
   if (error) {
@@ -79,6 +142,17 @@ const AdminDashboard: React.FC = () => {
       </h1>
 
       <ThesisFilters onFilter={handleFilterChange} />
+
+      {/* Ajout du bouton d'exportation */}
+      <div className="flex justify-end mb-4">
+        <button
+          onClick={exportToExcel}
+          className="px-6 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none"
+        >
+          Export to Excel
+        </button>
+      </div>
+
       {theses.length === 0 ? (
         <p className="text-center text-gray-600">No matching theses.</p>
       ) : (
@@ -114,7 +188,11 @@ const AdminDashboard: React.FC = () => {
             </thead>
             <tbody>
               {theses.map((thesis) => (
-                <tr key={thesis.id} className="hover:bg-gray-100">
+                <tr
+                  key={thesis.id}
+                  className="hover:bg-gray-100"
+                  onClick={() => handleThesisClick(thesis.id)}
+                >
                   <td className="border border-gray-300 px-4 py-2">
                     {thesis.advisor
                       ? `${thesis.advisor.firstname} ${thesis.advisor.lastname}`
@@ -150,6 +228,49 @@ const AdminDashboard: React.FC = () => {
             </tbody>
           </table>
         </div>
+      )}
+
+      {/* Affichage de la modal si une thèse est sélectionnée */}
+      {showModal && selectedThesisId && (
+        <EntityDetailsModal
+          entityId={selectedThesisId}
+          entityName="Thesis"
+          apiEndpoints={{
+            entity: "/thesis",
+          }}
+          fields={[
+            { label: "Topic", value: "topic" },
+            { label: "Year", value: "year" },
+            { label: "Domain", value: "domain" },
+            { label: "Scientist Interest", value: "scientistInterest" },
+            { label: "Keyword", value: "keyword" },
+            { label: "advisor's firtname", value: "advisor.firstname" },
+            { label: "Advisor's Lastname", value: "advisor.lastname" },
+            { label: "Advisor's Cost Center", value: "advisor.costCenter" },
+            { label: "Advisor's IFRS", value: "advisor.ifrs" },
+            { label: "Advisor's Department", value: "advisor.department" },
+            {label: "Advisor's research area", value: "advisor.research_area"},
+            /*{ label: "Advisor's Establishment Name", value: "advisor.establishment.name" },
+            { label: "Advisor's Establishment Address", value: "advisor.establishment.address" },
+            { label: "Advisor's Establishment City", value: "advisor.establishment.city" },
+            { label: "Advisor's Establishment SIRET", value: "advisor.establishment.siret" },
+            { label: "Advisor's Establishment Zipcode", value: "advisor.establishment.zipcode" },
+            { label: "Advisor's Establishment Telephone", value: "advisor.establishment.telephone" },*/
+            { label: "Candidate's Firstname", value: "candidate.firstname" },
+            { label: "Candidate's Lastname", value: "candidate.lastname" },
+            { label: "Candidate's Birthdate", value: "candidate.birthdate" },
+            { label: "Candidate's Last Degree", value: "candidate.lastDegree" },
+            { label: "Candidate's Last Degree Date", value: "candidate.dateLastDegree" },
+            { label: "Laboratory Name", value: "laboratory.name" },
+            { label: "Laboratory Address", value: "laboratory.address" },
+            { label: "Laboratory City", value: "laboratory.city" },
+            { label: "Laboratory Country", value: "laboratory.country" },
+            { label: "Laboratory Means", value: "laboratory.means" },
+            { label: "Laboratory Expertise", value: "laboratory.expertise" },
+          ]}
+          onClose={handleCloseModal}
+          onEntityUpdated={handleEntityUpdated}
+        />
       )}
     </div>
   );
