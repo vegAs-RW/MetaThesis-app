7ab9cfeb0c9235d79936ce32826e046134091a42
vegAs-RW <jordan.pouchot2@gmail.com>
2024-11-20 14:37:06 +0100

    :bulb: Remove comment & fields traduction

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,36 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import api from '../services/api';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import api from "../services/api";
+import { useNavigate } from "react-router-dom";
 
 const SignupPage: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [department, setDepartment] = useState('');
-  const [researchArea, setResearchArea] = useState('');
-  const [ifrs, setIfrs] = useState('');
-  const [costCenter, setCostCenter] = useState('');
-  const [establishments, setEstablishments] = useState([]); // Liste des établissements
-  const [selectedEstablishment, setSelectedEstablishment] = useState(''); // Établissement sélectionné
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [department, setDepartment] = useState("");
+  const [researchArea, setResearchArea] = useState("");
+  const [ifrs, setIfrs] = useState("");
+  const [costCenter, setCostCenter] = useState("");
+  const [establishments, setEstablishments] = useState([]);
+  const [selectedEstablishment, setSelectedEstablishment] = useState("");
   const navigate = useNavigate();
 
   const fetchEstablishments = async () => {
     try {
-      const response = await api.get('/establishment'); // Requête vers l'API pour obtenir les établissements
-      setEstablishments(response.data.data); // Mise à jour de la liste des établissements
-      console.log("Établissements récupérés :", response.data.data);
+      const response = await api.get("/establishment");
+      setEstablishments(response.data.data);
     } catch (error) {
-      console.error("Erreur lors de la récupération des établissements :", error);
+      console.error(
+        "Erreur lors de la récupération des établissements :",
+        error
+      );
     }
   };
 
   useEffect(() => {
-    fetchEstablishments(); 
+    fetchEstablishments();
   }, []);
 
   const handleSignup = async (e: React.FormEvent) => {
-    e.preventDefault(); 
+    e.preventDefault();
 
     try {
       console.log("Données d'inscription :", {
@@ -44,20 +46,20 @@ const SignupPage: React.FC = () => {
         costCenter,
         selectedEstablishment,
       });
-      
-      await api.post('/auth/register', {
+
+      await api.post("/auth/register", {
         email,
         password,
         firstname,
         lastname,
         department,
-        research_area: researchArea, 
+        research_area: researchArea,
         ifrs,
         costCenter,
-        establishment: selectedEstablishment, 
+        establishment: selectedEstablishment,
       });
 
-      navigate('/login');
+      navigate("/login");
     } catch (error) {
       console.error("Erreur lors de l'inscription :", error);
     }
@@ -66,10 +68,14 @@ const SignupPage: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-blue-200 to-blue-500">
       <div className="w-full max-w-lg px-8 py-6 bg-white rounded-lg shadow-lg transform transition-all duration-500 hover:shadow-2xl md:px-10 md:py-8 lg:max-w-md">
-        <h1 className="text-3xl font-bold text-center text-blue-600 mb-8">Inscription</h1>
+        <h1 className="text-3xl font-bold text-center text-blue-600 mb-8">
+          Sign up
+        </h1>
         <form onSubmit={handleSignup} className="space-y-6">
           <div>
-            <label className="block text-md font-semibold text-gray-600">Email :</label>
+            <label className="block text-md font-semibold text-gray-600">
+              Email:
+            </label>
             <input
               type="email"
               value={email}
@@ -79,7 +85,9 @@ const SignupPage: React.FC = () => {
             />
           </div>
           <div>
-            <label className="block text-md font-semibold text-gray-600">Mot de passe :</label>
+            <label className="block text-md font-semibold text-gray-600">
+              Password:
+            </label>
             <input
               type="password"
               value={password}
@@ -89,7 +97,9 @@ const SignupPage: React.FC = () => {
             />
           </div>
           <div>
-            <label className="block text-md font-semibold text-gray-600">Prénom :</label>
+            <label className="block text-md font-semibold text-gray-600">
+              Firstname:
+            </label>
             <input
               type="text"
               value={firstname}
@@ -99,7 +109,9 @@ const SignupPage: React.FC = () => {
             />
           </div>
           <div>
-            <label className="block text-md font-semibold text-gray-600">Nom :</label>
+            <label className="block text-md font-semibold text-gray-600">
+              Lastname:
+            </label>
             <input
               type="text"
               value={lastname}
@@ -109,7 +121,9 @@ const SignupPage: React.FC = () => {
             />
           </div>
           <div>
-            <label className="block text-md font-semibold text-gray-600">Organisation :</label>
+            <label className="block text-md font-semibold text-gray-600">
+              Organization:
+            </label>
             <input
               type="text"
               value={department}
@@ -119,7 +133,9 @@ const SignupPage: React.FC = () => {
             />
           </div>
           <div>
-            <label className="block text-md font-semibold text-gray-600">Service :</label>
+            <label className="block text-md font-semibold text-gray-600">
+              Service:
+            </label>
             <input
               type="text"
               value={researchArea}
@@ -129,7 +145,9 @@ const SignupPage: React.FC = () => {
             />
           </div>
           <div>
-            <label className="block text-md font-semibold text-gray-600">Code IFRS :</label>
+            <label className="block text-md font-semibold text-gray-600">
+              IFRS code:
+            </label>
             <input
               type="text"
               value={ifrs}
@@ -139,7 +157,9 @@ const SignupPage: React.FC = () => {
             />
           </div>
           <div>
-            <label className="block text-md font-semibold text-gray-600">Centre de coût :</label>
+            <label className="block text-md font-semibold text-gray-600">
+              Cost center:
+            </label>
             <input
               type="text"
               value={costCenter}
@@ -149,26 +169,30 @@ const SignupPage: React.FC = () => {
             />
           </div>
           <div>
-            <label className="block text-md font-semibold text-gray-600">Établissement :</label>
+            <label className="block text-md font-semibold text-gray-600">
+              Establishment:
+            </label>
             <select
               value={selectedEstablishment}
               onChange={(e) => setSelectedEstablishment(e.target.value)}
               required
               className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
             >
-              <option value="">Sélectionner un établissement</option>
-              {establishments.map((establishment: { id: string; name: string }) => (
-                <option key={establishment.id} value={establishment.id}>
-                  {establishment.name}
-                </option>
-              ))}
+              <option value="">Select an establishment</option>
+              {establishments.map(
+                (establishment: { id: string; name: string }) => (
+                  <option key={establishment.id} value={establishment.id}>
+                    {establishment.name}
+                  </option>
+                )
+              )}
             </select>
           </div>
           <button
             type="submit"
             className="w-full py-3 mt-4 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-300 shadow-lg focus:ring-4 focus:ring-blue-400 focus:outline-none"
           >
-            S'inscrire
+            Signup
           </button>
         </form>
       </div>
