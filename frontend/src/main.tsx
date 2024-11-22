ab2ab12161f475502c42a71795d9e83535049921
vegAs-RW <jordan.pouchot2@gmail.com>
2024-11-22 15:06:26 +0100

    :wrench: Update store for persistance

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import store from './redux/store.ts'
+import {store, persistor} from './redux/store.ts'
 import App from './App.tsx'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,14 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 import authReducer from "./authSlice";
+
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
     reducer: {
-        auth: authReducer,
+        auth: persistedReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: ['persist/PERSIST'],
+            ignoredPaths: ['register'],
+          },
+        }),
     });
 
+    const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 
-export default store;
+export { store, persistor };
